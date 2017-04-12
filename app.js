5fcf4ef32faaed74ac8f4cf6b3b2cb263f10b7ec
ImaY <wx51yz@qq.com>
2017-04-12 17:18:40 +0800

    ui_router
    samples

---
+++ app.js
@@ -0,0 +1,36 @@
+var photoGallery = angular.module('photoGallery', ['ui.router']);
+
+photoGallery.config(function($stateProvider, $urlRouterProvider) {
+    // $urlRouterProvider 负责处理在状态配置中指定的url路由方式之外的 url 请求的路由方式。
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+        .state('content', {
+            url: '/',
+            views: {
+                "": {
+                    templateUrl: 'partials/content.html'
+                },
+                "header@content": { templateUrl: 'partials/header.html' }
+            }
+        })
+        .state('content.home', {
+            url: 'home',
+            views: {
+                "body@content": { templateUrl: 'partials/home.html' }
+            }
+        })
+        .state('content.photos', {
+            url: '/photos',
+            views: {
+                "body@content": { templateUrl: 'partials/photos.html' }
+            }
+        })
+        .state('content.about', {
+            url: '/about',
+            views: {
+                "body@content": { templateUrl: 'partials/about.html' }
+            }
+        })
+
+})
